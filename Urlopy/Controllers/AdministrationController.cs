47bbc4108d7cd3246b22d94329cc4606a13e396e
quantic92 <mati9.7.1992@wp.pl>
2015-05-30 16:04:47 +0200

    Łowicz vol. Chujwieile

--- Urlopy/Controllers/AdministrationController.cs
+++ Urlopy/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,6 +22,47 @@ namespace Urlopy.Controllers
         {
             var employeeList = db.Users.ToList<ApplicationUser>();
             return View(employeeList);
+        }
+        public ActionResult ManageUserRoles()
+        {
+            var temp = db.Users.ToList<ApplicationUser>();
+            var temp2 = db.Roles.ToList<Microsoft.AspNet.Identity.EntityFramework.IdentityRole>();
+            ViewBag.UsersList = temp;
+            ViewBag.RolesList = temp2;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ManageUserRoles(FormCollection form)
+        {
+            if(form["id"] == "1")
+            { 
+                string userId = form["user"];
+                string userRole = form["role"];
+                var account = new AccountController();
+                IdentityResult x = account.UserManager.AddToRole(userId, userRole);
+            }
+            if(form["id"] == "2")
+            {
+                string userId = form["user2"];
+                string userRole = form["role2"];
+                var account = new AccountController();
+                if (account.UserManager.IsInRole(userId, userRole))
+                { 
+                IdentityResult x = account.UserManager.RemoveFromRole(userId, userRole);
+                }
+            }
+            if(form["id"] == "3")
+            {
+                string userId = form["user3"];
+                var account = new AccountController();
+                ViewBag.RolesForThisUser = account.UserManager.GetRoles(userId);
+            }
+            var temp = db.Users.ToList<ApplicationUser>();
+            var temp2 = db.Roles.ToList<Microsoft.AspNet.Identity.EntityFramework.IdentityRole>();
+            ViewBag.UsersList = temp;
+            ViewBag.RolesList = temp2;
+
+            return View();
         }
 	}
 }
