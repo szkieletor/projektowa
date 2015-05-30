d09c287f55a5c741e8b45d6359e8d7f6bf9a8fcf
szkieletor <quenmatt129@gmail.com>
2015-05-30 16:15:44 +0200

    dodawanie urlopow niedokonczone

---
+++ Urlopy/Models/UserProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+namespace Urlopy.Models
+{
+    public class UserProvider
+    {
+        public UserManager<ApplicationUser> UserManager { get; set; }
+        public UserProvider()
+            : this(new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext())))
+        {
+
+        }
+
+        public UserProvider(UserManager<ApplicationUser> userManager)
+        {
+            // TODO: Complete member initialization
+            UserManager = userManager;
+        }
+    }
+}
